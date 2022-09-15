b2670559113ce0b55b6566ae80c61de66a23213c
Adryancosmo56@gmail.com <Vikin.021>
2022-09-14 22:31:25 -0300

    Autualização Interface

--- interface.py
+++ interface.py
@@ -1,4 +1,13 @@
 from PySimpleGUI import PySimpleGUI as sg
+from experta import *
+import func
+import api
+
+from regras import *
+
+temp = api.temp
+umidadeAr = api.humidity
+condClima = api.condClima
 
 
 def janela_main():
@@ -20,7 +29,7 @@ def janela_resul():
         [sg.Text('Resposta Expert System')],
         [sg.Text('', key='resul1')],
         [sg.Text('', key='resul2')],
-        [sg.Button('Voltar a tela inicial'), sg.Push(), sg.Button('Finalizar')]
+        [sg.Button('Voltar'), sg.Push(), sg.Button('Finalizar')]
     ]
     return sg.Window('Expert System', layout=layout, finalize=True)
 
@@ -36,12 +45,37 @@ while True:
     if window == janela1 and event == 'Verificar Irrigação':
         aux = values['umidadeSolo']
         aux2 = values['alturaPlanta']
-
         janela2 = janela_resul()
+        engine.reset()
+        engine.declare(Fact(TemperaturaAcima30=func.temperaturaAcima30(temp),
+                            Temperatura20_30=func.temperatura20_30(temp),
+                            Temperatura10_20=func.temperatura10_20(temp),
+                            TemperaturaMinima=func.temperaturaMinima(temp),
+                            UmidadeArAbaixo50=func.umidadeArAbaixo50(
+                                umidadeAr),
+                            UmidadeArAbaixo30=func.umidadeArAbaixo30(
+                                umidadeAr),
+                            UmidadeAr=func.umidadeAr50(umidadeAr),
+                            TempChuva=func.tempChuva(condClima),
+                            UmidadeSoloAcima60=func.UmidadeSoloAcima60(aux),
+                            UmidadeSoloAbaixo60=func.UmidadeSoloAbaixo60(aux),
+                            PlantaAcima6=aux2
+                            ))
+        engine.run()
+        resultado = ret()
+        resultadoP = retP()
+        janela2['resul1'].update(f'{resultado}')
+        if(resultadoP == None):
+            janela2['resul2'].update('Planta em fase de crescimento!')
+        else:
+            janela2['resul2'].update(f'{resultadoP}')
         janela1.hide()
-    if window == janela2 and event == 'Voltar a tela inicial':
+
+    if window == janela2 and event == 'Voltar':
+        engine.reset()
         janela2.hide()
         janela1.un_hide()
+
     if window == janela2 and event == 'Finalizar' or event == sg.WIN_CLOSED:
         break
 
--- regras.py
+++ regras.py
@@ -1,13 +1,15 @@
-
-from unittest import result
 from experta import *
-import func
-import api
-import interface
 
-temp = api.temp
-umidadeAr = api.humidity
-condClima = api.condClima
+retorno = None
+retornoP = None
+
+
+def ret():
+    return retorno
+
+
+def retP():
+    return retornoP
 
 
 class InferenceEngine(KnowledgeEngine):
@@ -17,28 +19,33 @@ class InferenceEngine(KnowledgeEngine):
     @Rule(Fact(TempChuva=True), Fact(UmidadeAr=True))
     def Chuva(self):
         self.declare(Fact(IrrigacaooChuva=True))
-        print("Clima chuvoso - não irrigar")
+        global retorno
+        retorno = ("Clima chuvoso - não irrigar")
 
     # Regra 2
     # Se chover porem a umidade do ar estiver baixa então irrigar menos
     @Rule(Fact(TempChuva=True), Fact(UmidadeArAbaixo50=True))
     def poucaChuva(self):
         self.declare(Fact(IrrigacaoPoucaChuva=True))
-        print("Pouco chuva e Umidade baixa - irrigar com frequencia diminida pela metade")
+        global retorno
+        retorno = (
+            "Pouco chuva e Umidade baixa - irrigar com frequencia diminida pela metade")
 
     # Regra 3
     # Se a temperatura estiver acima de 30° irrigar de 2 em 2 horas.
     @Rule(Fact(TemperaturaAcima30=True), Fact(TempChuva=False))
     def climaQuente(self):
         self.declare(Fact(IrrigacaoClimaQuente=True), Fact(TempChuva=False))
-        print("Temperatura alta - Irigação será ligada de 2 em 2 horas!")
+        global retorno
+        retorno = ("Temperatura alta - Irigação será ligada de 2 em 2 horas!")
 
     # Regra 4
     # Se a temperatura estiver acima de 30° e umidade do ar abaixo de 30% então dobrar frequência da irrigação
     @Rule(Fact(TemperaturaAcima30=True), Fact(UmidadeArAbaixo30=True), Fact(TempChuva=False))
     def superAquecido(self):
         self.declare(Fact(IrrigacaoDobrada=True))
-        print("Ambiente superAquecido-Irrigação será ligada de 1 em 1 hora!")
+        global retorno
+        retorno = ("Ambiente superAquecido-Irrigação será ligada de 1 em 1 hora!")
 
     # Regra 5
     # se a temperatura estiver entre 20° e 30° e a umidade do ar estiver abaixo de 50 % então irrigue de 2 em 2 horas
@@ -51,14 +58,16 @@ class InferenceEngine(KnowledgeEngine):
     @Rule(Fact(Temperatura20_30=True), Fact(UmidadeAr=True), Fact(TempChuva=False))
     def TempoIdeal1(self):
         self.declare(Fact(IrrigacaoBaixa=True))
-        print("Temperatura ideal - irrigar com frequencia diminuida")
+        global retorno
+        retorno = ("Temperatura ideal - irrigar com frequencia diminuida")
 
     # Regra 7
     # Se a temperatura estiver abaixo de 10° então não irrigar
     @Rule(Fact(TemperaturaMinima=True), Fact(TempChuva=False))
     def TempoFrio(self):
         self.declare(Fact(IrrigacaoFrio=True))
-        print("Clima muito frio - Não irrigar")
+        global retorno
+        retorno = "Clima muito frio - Não irrigar"
 
     # Regra 8
     # se a temperatura estiver entre 10° e 20° então irrigue só de manhã uma vez./
@@ -71,64 +80,50 @@ class InferenceEngine(KnowledgeEngine):
     @ Rule(Fact(Temperatura10_20=True), Fact(UmidadeAr=True), Fact(TempChuva=False))
     def Manha(self):
         self.declare(Fact(IrrigacaoManha=False))
-        print("Temperatura baixa - Não será ligada a irrigação!")
+        global retorno
+        retorno = 'Temperatura Baixa - Não irá ligar irrigação'
 
     # Regra 10
     # se a regra 5 for verdadeira verificar a condição do solo
+
     @Rule(Fact(IrrigacaoNormal=True), Fact(UmidadeSoloAcima60=True))
     def SoloUmido(self):
         self.declare(Fact(IrrigacaoNormalFinal=False))
-        print("Solo Muito umido - Não irrigar")
+        global retorno
+        retorno = ("Solo Muito umido - Não irrigar")
 
     # Regra 11
     @Rule(Fact(IrrigacaoNormal=True), Fact(UmidadeSoloAbaixo60=True))
     def SoloSeco(self):
         self.declare(Fact(IrrigacaoNormalFinal=True))
-        print("Solo seco - Ligar irrigação")
+        global retorno
+        retorno = ("Solo seco - Ligar irrigação")
 
     # Regra 12
     # se a regra 8 for verdadeira verificar condição do solo
     @Rule(Fact(IrrigacaoManha=True), Fact(UmidadeSoloAcima60=True))
     def SoloUmidoManha(self):
         self.declare(Fact(IrrigacaoManhaFinal=False))
-        print("Solo Muito umido - Não irrigar")
+        global retorno
+        retorno = ("Solo Muito umido - Não irrigar")
 
     # Regra 13
     @Rule(Fact(IrrigacaoManha=True), Fact(UmidadeSoloAbaixo60=True))
     def SoloSecoManha(self):
         self.declare(Fact(IrrigacaoManhaFinal=True))
-        print("Ligar irrigação só uma vez de manhã")
+        global retorno
+        retorno = ("Ligar irrigação só uma vez de manhã")
 
     # Regra 14
     @Rule(Fact(PlantaAcima6=True))
     def estagioFinal(self):
         self.declare(Fact(IrrigacaoMetade=True))
-        print("Planta no estagio final - irrigar com metade do volume")
+        global retornoP
+        retornoP = ("Planta no estagio final - irrigar com metade do volume")
 
 
 engine = InferenceEngine()
-engine.reset()
 
-engine.declare(Fact(TemperaturaAcima30=func.temperaturaAcima30(temp),
-                    Temperatura20_30=func.temperatura20_30(temp),
-                    Temperatura10_20=func.temperatura10_20(temp),
-                    TemperaturaMinima=func.temperaturaMinima(temp),
-                    UmidadeArAbaixo50=func.umidadeArAbaixo50(umidadeAr),
-                    UmidadeArAbaixo30=func.umidadeArAbaixo30(umidadeAr),
-                    UmidadeAr=func.umidadeAr50(umidadeAr),
-                    TempChuva=func.tempChuva(condClima),
-                    UmidadeSoloAcima60=func.UmidadeSoloAcima60(interface.aux),
-                    UmidadeSoloAbaixo60=func.UmidadeSoloAbaixo60(
-                        interface.aux),
-                    PlantaAcima6=interface.aux2
-                    ))
 
-print(func.temperaturaAcima30(temp), func.temperatura20_30(temp), func.umidadeArAbaixo50(umidadeAr),
-      func.umidadeArAbaixo30(umidadeAr), func.umidadeAr50(umidadeAr),
-      func.temperaturaMinima(temp), func.tempChuva(
-          condClima), func.UmidadeSoloAcima60(interface.aux),
-      func.UmidadeSoloAbaixo60(
-    interface.aux), interface.aux2)
 # Altura da planta
 # Quantidade de sol diaria (em horas)
-engine.run()
